7aef4884cff57838634916f4cd0a52ce9139fd44
Williest-Andry <williestnyainaandry@gmail.com>
2025-11-19 09:12:54 +0300

    feat: add Table.js

---
+++ Table.js
@@ -0,0 +1,20 @@
+class Table{
+    constructor(longueur, largeur, etat, couleur){
+        this.longueur = longueur;
+        this.largeur = largeur;
+        this.etat = etat;
+        this.couleur = couleur
+    }
+
+    getLongueur(){
+        return this.longueur;
+    }
+
+    getLargeur(){
+        return this.largeur
+    }
+
+    getSurface() {
+        return this.longueur * this.largeur;
+    }
+}
